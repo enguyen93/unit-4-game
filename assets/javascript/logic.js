6221558f62623f69ca20333766ede345f8968dd7
enguyen93 <enguyenusaf93@gmail.com>
2018-09-10 00:23:45 -0500

    added values to buttons

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -20,23 +20,38 @@ for (var i = 0; i < crystals.length; i++) {
 
     // 2. Create image tags
     var crystalBtn = $("<button>");
-
+    var values = ["5", "10", "15", "20"];
     // 3. adding attributes to the crystal images
     crystalBtn.attr("src", crystals[i]);
     crystalBtn.attr("id", crystalIds[i]);
     crystalBtn.attr("style", "width:50px; height:50px");
+    // crystalBtn.attr("data-num", values[i]);
     crystalBtn.addClass(crystalIds[i]);
-
     // 4. Finally, append each crystal images
     $("#crystals").append(crystalBtn);
 
 }
 
-$(".pointHolder").text("Your current points go here! " + points)
-
-    $(crystal1).on("click", function () {
-        points += 5
+$("#crystal1").on("click", function () {
+    points += 5;
+    $("#yourPoints").text(points);
 })
+
+$("#crystal2").on("click", function () {
+    points += 10;
+    $("#yourPoints").text(points);
+})
+
+$("#crystal3").on("click", function () {
+    points += 15;
+    $("#yourPoints").text(points);
+})
+
+$("#crystal4").on("click", function () {
+    points += 20;
+    $("#yourPoints").text(points);
+})
+
 
 
 
@@ -44,9 +59,8 @@ $(".pointHolder").text("Your current points go here! " + points)
 function compChoice() {
     var randScore = Math.floor(Math.random() * (101 - 35)) + 35;
     return randScore;
-
+    console.log(randScore);
 }
-
 
 $("#randScore").text(compChoice());
 
